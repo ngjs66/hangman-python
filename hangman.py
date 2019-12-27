86a095a22dc8c6a1ca536a7294946053216412d1
ngjs <jun.shuo95@gmail.com>
2019-12-27 22:23:19 +0000

    Cleaned up indentation of comments

--- hangman.py
+++ hangman.py
@@ -40,62 +40,61 @@ word = "secret"
 
 # creates an variable with an empty value
 guesses = ''
-print("The amount of characters in the word is %s." % len(word))
+print("The amount of characters in the word is %s." % len(word)) # hints length of character of secret word
 
 # determine the number of turns
 turns = 10
 
 # while loop checks if the turns are more than zero
 while turns > 0:
-# make a counter that starts with zero
+    # make a counter that starts with zero
     failed = 0
 
-# for every character in secret_word
+    # for every character in secret_word
     for char in word:
 
-# see if the character is in the players guess
+        # see if the character is in the players guess
         if char in guesses:
 
-# print then out the character
+            # print then out the character
             print(char)
 
         else:
 
-# if not found, print a dash
+            # if not found, print a dash
             print("_"),
 
-# and increase the failed counter with one
+    # and increase the failed counter with one
     failed += 1
 
-# if failed is equal to zero
-
-# print You Won
+    # if failed is equal to zero
     if failed == 0:
+        # print You Won
         print("You won \n")
 
-# exit the script
+        # exit the script
         break
 
-# ask the user go guess a character
+    # ask the user go guess a character
     guess = input("Guess a character: \n")
 
-# set the players guess to guesses
+    # set the players guess to guesses
     guesses += guess
 
-# if the guess is not found in the secret word
+    # if the guess is not found in the secret word
     if guess not in word:
 
-# turns counter decreases with 1 (now 9)
+        # turns counter decreases with 1 (now 9)
         turns -= 1
 
-# print wrong
+        # print wrong
         print("Wrong! \n")
 
-# how many turns are left
+        # how many turns are left
         print ("You have", + turns, "more guesses")
 
-# if the turns are equal to zero
+        # if the turns are equal to zero
         if turns == 0:
 
-# print "You Loose"
+            # print "You Loose"
             print("You lose!")
